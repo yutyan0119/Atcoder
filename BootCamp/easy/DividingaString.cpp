61d38f7c606bc92582ed4d8e77ff1029d8302b82
Yuto Nakamura <maron07222010@gmail.com>
2021-10-21 22:45:30 +0900

    DividingStringは復習したい

--- BootCamp/easy/DividingaString.cpp
+++ BootCamp/easy/DividingaString.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T>
-bool chmin(T &a, const T& b) {
+bool chmin(T& a, const T& b) {
   if (a > b) {
     a = b;  // aをbで更新
     return true;
@@ -11,7 +11,7 @@ bool chmin(T &a, const T& b) {
 }
 
 template <typename T>
-bool chmax(T &a, const T& b) {
+bool chmax(T& a, const T& b) {
   if (a < b) {
     a = b;  // aをbで更新
     return true;
@@ -19,11 +19,26 @@ bool chmax(T &a, const T& b) {
   return false;
 }
 
-#define rep(i, s, e) for(int i = s; i <e ; i++)
+#define rep(i, s, e) for (int i = s; i < e; i++)
 
 /* ---------------------------------------------------*/
 
 int main() {
   string S;
   cin >> S;
+  int N = (int)S.size();
+  vector<vector<int>> dp(N + 1, vector<int>(3, -1));
+  dp[0][0] = 0;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j <= min(i, 2); ++j) {
+      string suffix = S.substr(i - j, j);
+      for (int add = 1; add <= 2 && i + add <= N; ++add) {
+        // 同じものが連続しないなら更新
+        if (S.substr(i, add) != suffix) {
+          chmax(dp[i + add][add], dp[i][j] + 1);
+        }
+      }
+    }
+  }
+  cout << max(dp[N][1], dp[N][2]) << endl;
 }
