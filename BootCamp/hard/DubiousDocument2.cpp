e72f7b1048dddd2bb9d999f42a9c2405607533e4
Yuto Nakamura <maron07222010@gmail.com>
2021-11-16 22:41:42 +0900

    むずい

--- BootCamp/hard/DubiousDocument2.cpp
+++ BootCamp/hard/DubiousDocument2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T>
-bool chmin(T &a, const T& b) {
+bool chmin(T& a, const T& b) {
   if (a > b) {
     a = b;  // aをbで更新
     return true;
@@ -11,7 +11,7 @@ bool chmin(T &a, const T& b) {
 }
 
 template <typename T>
-bool chmax(T &a, const T& b) {
+bool chmax(T& a, const T& b) {
   if (a < b) {
     a = b;  // aをbで更新
     return true;
@@ -19,14 +19,42 @@ bool chmax(T &a, const T& b) {
   return false;
 }
 
-#define rep(i, s, e) for(int i = s; i <e ; i++)
+#define rep(i, s, e) for (int i = s; i < e; i++)
 
 typedef long long ll;
 
 /* ---------------------------------------------------*/
 
 int main() {
-  string Sf, T;
+  string Sf, T, ans;
   cin >> Sf >> T;
-  
+  int n = (int)Sf.size();
+  int m = (int)T.size();
+  bool exist = false;
+  for (int i = n - 1; i >= 0; i--) {
+    bool can = true;
+    for (int j = 0; j < m; j++) {
+      if (Sf.at(i - m + 1 + j) != '?' && Sf.at(i - m + 1 + j) != T.at(j)) {
+        can = false;
+      }
+    }
+    if (can) {
+      string S = Sf;
+      for (int j = i - m + 1; j <= i; j++) {
+        S.at(j) = T.at(j - (i - m + 1));
+      }
+      for (int j = 0; j < n; j++) {
+        if (S.at(j) == '?') {
+          S.at(j) = 'a';
+        }
+      }
+      cout << S << endl;
+      exist = true;
+      break;
+    }
+  }
+  if (!exist) {
+    cout << "UNRESTORABLE" << endl;
+  }
+  return 0;
 }
