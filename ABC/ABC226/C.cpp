71725fa711d61a5120a35c4b18a8046f5d59ceac
Yuto Nakamura <maron07222010@gmail.com>
2021-11-21 14:37:58 +0900

    グラフ問題初挑戦

--- ABC/ABC226/C.cpp
+++ ABC/ABC226/C.cpp
@@ -21,9 +21,34 @@ bool chmax(T& a, const T& b) {
 
 #define rep(i, s, e) for (int i = s; i < e; i++)
 
+typedef long long ll;
 /* ---------------------------------------------------*/
 
+#define N 200100
+ll t[N];
+ll k[N];
+vector<int> e[N];  //!=e(N),配列をN個用意してます。
+bool used[N] = {false};
 int main() {
-  int N ;
-  
+  int n, x;
+  ll ans = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> t[i] >> k[i];
+    for (int j = 0; j < k[i]; j++) {
+      cin >> x;               // A_ijを受け取り
+      e[i].push_back(x - 1);  // e[i]にインデックスをあわせて入れる
+    }
+  }
+  used[n - 1] = true;  //まずn-1は到達可能
+  for (int i = n - 1; i >= 0; i--) { //順番に後ろから到達可能なところをたどっていく
+    if (used[i]) {
+      ans += t[i];
+      for (int j = 0; j < k[i]; j++) {
+        used[e[i][j]] = true; //絶対ちっさい番号なのでモーマンタイ
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
 }
