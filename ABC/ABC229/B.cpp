147fbe6afdcff8b457d03736377f4b59de214e63
Yuto Nakamura <maron07222010@gmail.com>
2021-11-30 07:26:15 +0900

    しゃくとり法

--- ABC/ABC229/B.cpp
+++ ABC/ABC229/B.cpp
@@ -21,11 +21,31 @@ bool chmax(T& a, const T& b) {
 
 #define rep(i1, s, e) for (int i1 = s; i1 < e; i1++)
 
+typedef long long ll;
+
 /* ---------------------------------------------------*/
 
 int main() {
   string S;
   int K;
   cin >> S >> K;
-  
+  int N = (int)S.size();
+  vector<int> ss(N);
+  for (int i = 0; i < N; i++) {
+    ss.at(i) = (S.at(i) == '.'?1:0);
+  }
+  ll ans = 0;
+  ll r = 0;
+  ll sum = 0;
+  for (ll l = 0; l < N; l++)
+  {
+    while (r < N && sum+ss[r] <= K)
+    { 
+      sum += ss.at(r);
+      r++;
+    }
+    ans = max(ans,r-l);
+    sum -= ss.at(l);
+  }
+  cout << ans << endl;
 }
