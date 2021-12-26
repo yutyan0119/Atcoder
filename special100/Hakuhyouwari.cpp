83a90d5b179ac8f4ad318a19f5187d733fe6060f
Yuto Nakamura <maron07222010@gmail.com>
2021-12-26 23:33:19 +0900

    dfsは復習が必要そう

--- special100/Hakuhyouwari.cpp
+++ special100/Hakuhyouwari.cpp
@@ -29,25 +29,38 @@ vector<vector<int>> field;
 int maxcount = 0;
 int dy[4] = {0, 1, 0, -1};
 int dx[4] = {1, 0, -1, 0};
-vector<vector<bool>> seen;
 
-void dfs(vector<vector<int>> G, int y, int x, int& count) {}
+void dfs(vector<vector<int>> G, int y, int x, int count) {
+  G[y][x] = 0;
+  for (int i = 0; i < 4; i++) {
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+    if (G[ny][nx] == 1) {
+      dfs(G, ny, nx, count + 1);
+    }
+  }
+  G[y][x] = 1;
+  if (!(G[y + 1][x] || G[y][x + 1] || G[y - 1][x] || G[y][x - 1])) {
+    chmax(maxcount, count);
+    return;
+  }
+}
 
 int main() {
   cin >> m >> n;
-  field.assign(n, vector<int>(m));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  field.assign(n + 2, vector<int>(m + 2, 0));
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
       cin >> field[i][j];
     }
   }
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      int count = 1;
+      if (field[i][j] == 0) continue;
+      dfs(field, i, j, count);
+      chmax(maxcount, count);
     }
-    
   }
-  
+  cout << maxcount << endl;
 }
