254cb7e1d1b2ad11b97de3772129fbc0fe4f4eb2
Yuto Nakamura <maron07222010@gmail.com>
2022-02-05 20:53:55 +0900

    directory rename

---
+++ ABC/ABC238/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+#define rep(i1, s, e) for (int i1 = s; i1 < e; i1++)
+
+typedef long long ll;
+
+/* ---------------------------------------------------*/
+
+int main() {
+  
+}
