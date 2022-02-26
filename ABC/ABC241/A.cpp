0ae4eec93da092240de4802b2278d8f402adfb46
Yuto Nakamura <maron07222010@gmail.com>
2022-02-27 00:10:29 +0900

    ABC241join

---
+++ ABC/ABC241/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+#define rep(i, s, e) for (int i = s; i < e; i++)
+
+typedef long long ll;
+/* ---------------------------------------------------*/
+
+int main() {
+  vector<int> a(10);
+  for (int i = 0; i < 10; i++) {
+    cin >> a[i];
+  }
+
+  cout << a[a[a[0]]] << endl;
+}
