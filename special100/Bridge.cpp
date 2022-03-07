16182e18e779fa982f2655eac9b2521596610f8c
Yuto Nakamura <maron07222010@gmail.com>
2022-03-07 23:03:07 +0900

    今日の精進

---
+++ special100/Bridge.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+#define rep(i, s, e) for (int i = s; i < e; i++)
+
+typedef long long ll;
+
+struct UnionFind {
+  vector<int> par, siz;
+  UnionFind(int n) {
+    par.assign(n, -1);
+    siz.assign(n, 1);
+  }
+  int root(int x) {
+    if (par[x] == -1)
+      return x;
+    else
+      return par[x] = root(par[x]);
+  }
+
+  bool issame(int x, int y) { return root(x) == root(y); }
+
+  bool unite(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if (x == y) return false;
+    if (siz[x] < siz[y]) {
+      swap(x, y);
+    }
+    par[y] = x;
+    siz[x] += siz[y];
+    return true;
+  }
+
+  int size(int x) { return siz[x]; }
+};
+
+/* ---------------------------------------------------*/
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M);
+  vector<int> B(M);
+  for (int i = 0; i < M; i++) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    A[i] = a;
+    B[i] = b;
+  }
+  int ans = 0;
+  for (int i = 0; i < M; i++) {
+    UnionFind u(N);
+    for (int j = 0; j < M; j++) {
+      if (j != i) {
+        u.unite(A[j], B[j]);
+      }
+    }
+    int c = 0;
+    for (int j = 0; j < N; j++) {
+      if (j == u.root(j)) c++;
+    }
+    if (1 < c) ans++;
+  }
+  cout << ans << endl;
+}
